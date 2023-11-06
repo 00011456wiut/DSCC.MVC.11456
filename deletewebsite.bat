73fd342646d47df8a2834ec603c9d8cfb7b845ad
00011456 <isakovmansur021@gmail.com>
2023-11-06 23:24:35 +0500

    fix: ci

--- deletewebsite.bat
+++
@@ -1,9 +0,0 @@
-﻿%SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -command "Set-ExecutionPolicy Unrestricted -Force"
-
-
-IF NOT EXIST c:\temp mkdir c:\temp 
-cd c:\temp
-IF EXIST c:\temp\deletewebsite.ps1 %SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -command ".\deletewebsite.ps1"
-
-%SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -command "rm c:\inetpub\wwwroot\dscc-mvc-11456 -Recurse -Force"
-del /q "c:\temp\*.*"
--- installwebsite.bat
+++
@@ -1,7 +0,0 @@
-﻿%SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -command "Set-ExecutionPolicy Unrestricted -Force"
-
-IF NOT EXIST c:\inetpub\wwwroot\dscc-mvc-11456 mkdir c:\inetpub\wwwroot\dscc-mvc-11456
-
-cd c:\temp
-
-%SystemRoot%\sysnative\WindowsPowerShell\v1.0\powershell.exe -command ".\installwebsite.ps1"
